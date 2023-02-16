a407aa19d714cf7b7d2d7892e48d5505dbe0fa17
韦雪芳 <weixuefang@bluemoon.com.cn>
2023-02-16 21:39:38 +0800

    deploy *

---
+++ assets/component_Button.md.2458aafb.js
@@ -0,0 +1,41 @@
+import{V as t,_ as E,o as m,c as h,a as d,w as n,b as s,d as o,r as _}from"./app.18fffbd3.js";const{defineComponent:v}=t,B=v({name:"component-doc",components:{"render-demo-0":function(){const{createTextVNode:a,resolveComponent:c,withCtx:r,openBlock:p,createBlock:u}=t;function D(y,C){const i=c("a-button");return p(),u(i,{onClick:y.onClick},{default:r(()=>[a("\u6309\u94AE")]),_:1},8,["onClick"])}const{onMounted:e,ref:l,reactive:F}=t;return{render:D,...{setup(y,{expose:C}){C();const A={state:F({}),onClick:()=>{alert(1)},onMounted:e,ref:l,reactive:F};return Object.defineProperty(A,"__isScriptSetup",{enumerable:!1,value:!0}),A}}}}()}}),$=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u57FA\u7840\u7528\u6CD5","slug":"\u57FA\u7840\u7528\u6CD5","link":"#\u57FA\u7840\u7528\u6CD5","children":[]},{"level":2,"title":"Props","slug":"props","link":"#props","children":[]},{"level":2,"title":"Events","slug":"events","link":"#events","children":[]}],"relativePath":"component/Button.md"}');const f=s("h2",{id:"\u57FA\u7840\u7528\u6CD5",tabindex:"-1"},[o("\u57FA\u7840\u7528\u6CD5 "),s("a",{class:"header-anchor",href:"#\u57FA\u7840\u7528\u6CD5","aria-hidden":"true"},"#")],-1),k=s("div",null,[o("\u8BF7\u590D\u5236\u4E0B\u65B9\u793A\u4F8B\u4EE3\u7801"),s("code",null,"(demo\u4EE3\u7801\u7EF4\u62A4\u4E00\u822C\u4E3A\uFF1A\u5148\u5728docs/examples/**\u4E0B\u5199\u597D\u7EC4\u4EF6\u4F8B\u5B50\uFF0C\u518D\u642C\u5230\u4E0B\u9762vue\u4EE3\u7801\u4E2D\u3002\u56E0\u4E3Avue\u5355\u6587\u4EF6\u7EC4\u4EF6\u624D\u6709\u8BED\u6CD5\u63D0\u793A\uFF0C\u65B9\u4FBF\u5199\u4EE3\u7801)")],-1),x=s("div",{class:"language-vue"},[s("pre",{"v-pre":"",class:"shiki"},[s("code",null,[s("span",{class:"line"},[s("span",{style:{color:"#89DDFF"}},"<"),s("span",{style:{color:"#F07178"}},"template"),s("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#A6ACCD"}},"  "),s("span",{style:{color:"#89DDFF"}},"<"),s("span",{style:{color:"#F07178"}},"a-button"),s("span",{style:{color:"#89DDFF"}}," @"),s("span",{style:{color:"#C792EA"}},"click"),s("span",{style:{color:"#89DDFF"}},"="),s("span",{style:{color:"#89DDFF"}},'"'),s("span",{style:{color:"#A6ACCD"}},"onClick"),s("span",{style:{color:"#89DDFF"}},'"'),s("span",{style:{color:"#89DDFF"}},">"),s("span",{style:{color:"#A6ACCD"}},"\u6309\u94AE"),s("span",{style:{color:"#89DDFF"}},"</"),s("span",{style:{color:"#F07178"}},"a-button"),s("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#89DDFF"}},"</"),s("span",{style:{color:"#F07178"}},"template"),s("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),s("span",{class:"line"}),o(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#89DDFF"}},"<"),s("span",{style:{color:"#F07178"}},"script"),s("span",{style:{color:"#A6ACCD"}}," "),s("span",{style:{color:"#C792EA"}},"setup"),s("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),s("span",{class:"line"}),o(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#A6ACCD"}},"  "),s("span",{style:{color:"#89DDFF"}},"import"),s("span",{style:{color:"#A6ACCD"}}," "),s("span",{style:{color:"#89DDFF"}},"{"),s("span",{style:{color:"#F07178"}}," "),s("span",{style:{color:"#A6ACCD"}},"onMounted"),s("span",{style:{color:"#89DDFF"}},","),s("span",{style:{color:"#F07178"}}," "),s("span",{style:{color:"#A6ACCD"}},"ref"),s("span",{style:{color:"#89DDFF"}},","),s("span",{style:{color:"#F07178"}}," "),s("span",{style:{color:"#A6ACCD"}},"reactive"),s("span",{style:{color:"#F07178"}}," "),s("span",{style:{color:"#89DDFF"}},"}"),s("span",{style:{color:"#A6ACCD"}}," "),s("span",{style:{color:"#89DDFF"}},"from"),s("span",{style:{color:"#A6ACCD"}}," "),s("span",{style:{color:"#89DDFF"}},"'"),s("span",{style:{color:"#C3E88D"}},"vue"),s("span",{style:{color:"#89DDFF"}},"'")]),o(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#A6ACCD"}},"  "),s("span",{style:{color:"#C792EA"}},"const"),s("span",{style:{color:"#A6ACCD"}}," state "),s("span",{style:{color:"#89DDFF"}},"="),s("span",{style:{color:"#A6ACCD"}}," "),s("span",{style:{color:"#82AAFF"}},"reactive"),s("span",{style:{color:"#A6ACCD"}},"("),s("span",{style:{color:"#89DDFF"}},"{}"),s("span",{style:{color:"#A6ACCD"}},")")]),o(`
+`),s("span",{class:"line"}),o(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#A6ACCD"}},"  "),s("span",{style:{color:"#C792EA"}},"const"),s("span",{style:{color:"#A6ACCD"}}," onClick "),s("span",{style:{color:"#89DDFF"}},"="),s("span",{style:{color:"#A6ACCD"}}," "),s("span",{style:{color:"#89DDFF"}},"()"),s("span",{style:{color:"#A6ACCD"}}," "),s("span",{style:{color:"#C792EA"}},"=>"),s("span",{style:{color:"#A6ACCD"}}," "),s("span",{style:{color:"#89DDFF"}},"{")]),o(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#F07178"}},"    "),s("span",{style:{color:"#82AAFF"}},"alert"),s("span",{style:{color:"#F07178"}},"("),s("span",{style:{color:"#F78C6C"}},"1"),s("span",{style:{color:"#F07178"}},")")]),o(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#F07178"}},"  "),s("span",{style:{color:"#89DDFF"}},"}")]),o(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#89DDFF"}},"</"),s("span",{style:{color:"#F07178"}},"script"),s("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),s("span",{class:"line"}),o(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#89DDFF"}},"<"),s("span",{style:{color:"#F07178"}},"style"),s("span",{style:{color:"#A6ACCD"}}," "),s("span",{style:{color:"#C792EA"}},"lang"),s("span",{style:{color:"#A6ACCD"}},"="),s("span",{style:{color:"#89DDFF"}},'"'),s("span",{style:{color:"#C3E88D"}},"less"),s("span",{style:{color:"#89DDFF"}},'"'),s("span",{style:{color:"#A6ACCD"}}," "),s("span",{style:{color:"#C792EA"}},"scoped"),s("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#A6ACCD"}},"  "),s("span",{style:{color:"#89DDFF"}},"."),s("span",{style:{color:"#FFCB6B"}},"ant-btn"),s("span",{style:{color:"#89DDFF"}},"{")]),o(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#A6ACCD"}},"      "),s("span",{style:{color:"#B2CCD6"}},"color"),s("span",{style:{color:"#89DDFF"}},":"),s("span",{style:{color:"#A6ACCD"}}," red"),s("span",{style:{color:"#89DDFF"}},";")]),o(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#A6ACCD"}},"  "),s("span",{style:{color:"#89DDFF"}},"}")]),o(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#89DDFF"}},"</"),s("span",{style:{color:"#F07178"}},"style"),s("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),s("span",{class:"line"}),o(`
+`),s("span",{class:"line"})])])],-1),b=s("h2",{id:"props",tabindex:"-1"},[o("Props "),s("a",{class:"header-anchor",href:"#props","aria-hidden":"true"},"#")],-1),g=s("h2",{id:"events",tabindex:"-1"},[o("Events "),s("a",{class:"header-anchor",href:"#events","aria-hidden":"true"},"#")],-1);function V(a,c,r,p,u,D){const e=_("render-demo-0"),l=_("demo");return m(),h("div",null,[f,d(l,{customClass:"undefined",sourceCode:`<template>
+  <a-button @click="onClick">\u6309\u94AE</a-button>
+</template>
+
+<script setup>
+
+  import { onMounted, ref, reactive } from 'vue'
+  const state = reactive({})
+
+  const onClick = () => {
+    alert(1)
+  }
+<\/script>
+
+<style lang="less" scoped>
+  .ant-btn{
+      color: red;
+  }
+</style>
+
+`},{description:n(()=>[k]),highlight:n(()=>[x]),default:n(()=>[d(e)]),_:1}),b,g])}const M=E(B,[["render",V]]);export{$ as __pageData,M as default};
