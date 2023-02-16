fd0ec6a52cf04b8a345d4d05309464c2649b7a69
韦雪芳 <weixuefang@bluemoon.com.cn>
2023-02-16 21:42:31 +0800

    deploy *

---
+++ assets/component_index.md.871bf01a.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,c as n,e as a}from"./app.a257a9a2.js";const y=JSON.parse('{"title":"组件列表","description":"","frontmatter":{},"headers":[],"relativePath":"component/index.md"}'),r={name:"component/index.md"},d=a("",3),c=[d];function l(s,i,o,_,x,h){return e(),n("div",null,c)}const g=t(r,[["render",l]]);export{y as __pageData,g as default};
